b8c16abb6ddda5982b7ac186e34976e5b9aa7310
Jeetesh Mangwani <jeetesh.mangwani@gmail.com>
2014-07-15 14:13:20 +0530

    start taking command line arguments

--- resize_photos.sh
+++ resize_photos.sh
@@ -2,18 +2,60 @@
 
 # Author: Jeetesh Mangwani
 
-# Objective: Scale down photos so that they occupy less space
-# Prerequisites: ffmpeg package
+display_help() {
+    cat <<EOF
+Usage: ${0##*/} -s OUTPUT_SIZE -d DIRECTORY [-fvh] 
+Objective: Scale down photos so that they occupy less space
+Prerequisites: ffmpeg package
+-s desired output size: small, medium or large
+-d directory where the images reside
+-f image format: jpg, png, gif; if not specified then assumes all files are images
+-v display details as we progress
+EOF
+}
 
-ROOT_DIRECTORY=~/my-photos
-OUTPUT_SIZE=l # s: small, m: medium, l:large
+OPTIND=1
+DIRECTORY=""
+OUTPUT_SIZE="large"
+IMAGE_FORMAT=""
 
-if [ $OUTPUT_SIZE -eq s ]
-then
+while getopts "hs:d:f:v" opt; do
+    case $opt in
+	h)
+	    display_help
+	    exit 0
+	    ;;
+	s)
+	    OUTPUT_SIZE=$OPTARG
+	    ;;
+	d)
+	    DIRECTORY=$OPTARG
+	    ;;
+	f)
+	    IMAGE_FORMAT=$OPTARG
+	    ;;
+	v)
+	    VERBOSE=1
+	    ;;
+	'?')
+	    display_help
+	    exit 1
+	    ;;
+    esac
+done
+
+shift "$((OPTIND-1))" #remove remaining options on the command line
+	
+if [ -z $DIRECTORY ]; then
+ echo "Directory not specified"
+ display_help
+ exit 1
+fi
+
+if [ "$OUTPUT_SIZE" = "small" ]; then
     OUTPUT_WIDTH=640 #will be used if aspect ratio > 1.33
     OUTPUT_HEIGHT=480 #will be used if aspect ratio <= 1.33
-elif [ $OUTPUT_SIZE -eq m ]
-then
+elif [ "$OUTPUT_SIZE" = "medium" ]; then
     OUTPUT_WIDTH=800 #will be used if aspect ratio > 1.33
     OUTPUT_HEIGHT=600 #will be used if aspect ratio <= 1.33
 else
@@ -21,26 +63,40 @@ else
     OUTPUT_HEIGHT=750 #will be used if aspect ratio <= 1.33
 fi
 
-cd $ROOT_DIRECTORY
+cd $DIRECTORY
 #dirs=`ls | grep "20*"`
-dirs=./
+dirs=.
 
 for dir in  $dirs #list of directories
 do
 	echo 'Inside directory' $dir
 	cd $dir
-	files=`ls -1 | grep -e "jpg\|JPG"`
-	for file in $files
-	do
-		echo 'Dealing with' $dir '/' $file
-		
-		#for photos
-		ffmpeg -i $file -vf scale="'if(gt(a,4/3),$OUTPUT_WIDTH,-1)':'if(gt(a,4/3),-1,$OUTPUT_HEIGHT)'" -y $file
-		
-		#for videos
-		#sleep 30s #if we don't sleep, system overheats and shuts down
-		#ffmpeg -i $file -vf "scale=640:trunc(ow/a/2)*2, fps=24" -c:a copy -y r$file
-	done
+	files=""
+	if [ -z "$IMAGE_FORMAT" ]
+	then
+	    files=`ls -1`
+	else
+	    files=`ls -1 | grep -ie "$IMAGE_FORMAT"`
+	fi
+
+ if [ -z "$files" ]; then
+  echo "No $IMAGE_FORMAT images found"
+ else
+  for file in $files
+  do
+   echo 'Dealing with' $dir'/'$file
+   
+   scale="'if(gt(a,4/3),$OUTPUT_WIDTH,-1)':'if(gt(a,4/3),-1,$OUTPUT_HEIGHT)'"
+   if [ -z "$VERBOSE" ]
+   then
+       ffmpeg -i $file -vf scale=$scale -y $file 1>&2 2>/dev/null
+   else
+       ffmpeg -i $file -vf scale=$scale -y $file
+   fi
+
+  done
+ fi
+
 	cd ..
 done
 exit
