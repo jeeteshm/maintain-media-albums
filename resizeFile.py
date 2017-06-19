1b29b4c8fdb8b3f5cae1bb6e6bfe2a5338dd89cd
Jeetesh Mangwani <jeetesh.mangwani@gmail.com>
2017-06-18 22:03:27 -0700

    recognize the file type as image or video

--- resizeFile.py
+++ resizeFile.py
@@ -7,4 +7,20 @@ from os.path import expanduser;
 __author__ = "Jeetesh Mangwani"
 
 def resizeFile(filePath, args):
-    print("Dealing with the file: ", filePath);
+    name, extension = os.path.splitext(filePath);
+    image_file_extensions = [".png", ".jpg", ".jpeg"];
+    video_file_extensions = [".avi", ".mp4"];
+
+    if(extension in image_file_extensions):
+        resizeImageFile(filePath, args);
+    elif(extension in video_file_extensions):
+        resizeVideoFile(filePath, args);
+    else:
+        print("Ignoring the file: ", filePath);
+    
+
+def resizeImageFile(filePath, args):
+    print("Dealing with the image file: ", filePath);
+
+def resizeVideoFile(filePath, args):
+    print("Dealing with the video file: ", filePath);
