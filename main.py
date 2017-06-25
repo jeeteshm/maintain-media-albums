253361bed32bc9508338c297f12b0cfd4af88fc2
Jeetesh Mangwani <jeetesh.mangwani@gmail.com>
2017-06-25 15:49:10 -0700

    setup input and output directories

--- main.py
+++ main.py
@@ -9,20 +9,21 @@ __author__ = "Jeetesh Mangwani"
 
 def main():
     parser = argparse.ArgumentParser(description="This script compresses photos and videos in a given directory");
-    parser.add_argument("-d", "--directory", type=str, help="The directory that will be traversed for photos and videos", required=False, default=expanduser("~\my-photos"));
+    parser.add_argument("-id", "--inputdirectory", type=str, help="The input directory that will be traversed for photos and videos", required=False, default=expanduser("~\my-photos\input"));
+    parser.add_argument("-od", "--outputdirectory", type=str, help="The output directory where resized photos and videos will be put", required=False, default=expanduser("~\my-photos\output"));
     parser.add_argument("-is", "--imagesize", type=str, help="Output image size; one of 'small', 'medium' or 'large'", required=False, default="small");
     parser.add_argument("-vs", "--videosize", type=str, help="Output video size; one of 'small', 'medium', or 'large'", required=False, default="small");
     parser.add_argument("-v", "--verbose", help="Whether to output verbose output messages", required=False, default=False);
     args = parser.parse_args();
 
-    print("Root directory: ", args.directory);
+    print("Input directory: ", args.inputdirectory);
+    print("Output directory: ", args.outputdirectory);
     print("Output image size: ", args.imagesize);
     print("Output video size: ", args.videosize);
     print("Verbosity of log messages: ", args.verbose);
 
-    for path, subdirs, files in os.walk(args.directory):
+    for path, subdirs, files in os.walk(args.inputdirectory):
         for file in files:
-            filePath = os.path.join(path, file)
-            resizeFile(filePath, args)
+            resizeFile(path, subdirs, file, args)
 
 main();
--- resizeFile.py
+++ resizeFile.py
@@ -6,21 +6,28 @@ from os.path import expanduser;
 
 __author__ = "Jeetesh Mangwani"
 
-def resizeFile(filePath, args):
-    name, extension = os.path.splitext(filePath);
+def resizeFile(path, subdirs, file, args):
+    name, extension = os.path.splitext(file);
     image_file_extensions = [".png", ".jpg", ".jpeg"];
     video_file_extensions = [".avi", ".mp4"];
 
     if(extension in image_file_extensions):
-        resizeImageFile(filePath, args);
+        resizeImageFile(path, subdirs, file, args);
     elif(extension in video_file_extensions):
-        resizeVideoFile(filePath, args);
+        resizeVideoFile(path, subdirs, file, args);
     else:
-        print("Ignoring the file: ", filePath);
+        print("Ignoring the file: ", os.path.join(path, file));
     
 
-def resizeImageFile(filePath, args):
-    print("Dealing with the image file: ", filePath);
+def resizeImageFile(path, subdirs, file, args):
+    size = args.imagesize;
+    verbose = args.verbose;
+    filePath = os.path.join(path, file);
+    print("Resizing Image: {0} Size: {1}".format(filePath, size));
+    relativePath = os.path.relpath(filePath, args.inputdirectory);
+    destinationPath = os.path.join(args.outputdirectory, relativePath);
+    print(destinationPath);
+    
 
-def resizeVideoFile(filePath, args):
-    print("Dealing with the video file: ", filePath);
+def resizeVideoFile(path, subdirs, file, args):
+    print("Dealing with the video file: ", path);
