26fce6def0d38e41d7b83fff5155c604444d7c0b
Jeetesh Mangwani <jeetesh.mangwani@gmail.com>
2017-07-09 23:53:17 -0700

    set up image compression

--- resizeFile.py
+++ resizeFile.py
@@ -3,6 +3,7 @@
 import sys, os;
 import argparse;
 from os.path import expanduser;
+from subprocess import run;
 
 __author__ = "Jeetesh Mangwani"
 
@@ -23,11 +24,32 @@ def resizeImageFile(path, subdirs, file, args):
     size = args.imagesize;
     verbose = args.verbose;
     filePath = os.path.join(path, file);
-    print("Resizing Image: {0} Size: {1}".format(filePath, size));
-    relativePath = os.path.relpath(filePath, args.inputdirectory);
-    destinationPath = os.path.join(args.outputdirectory, relativePath);
-    print(destinationPath);
+    relativeFilePath = os.path.relpath(filePath, args.inputdirectory);
+    relativeDirectoryPath = os.path.relpath(path, args.inputdirectory);
+    destinationFilePath = os.path.join(args.outputdirectory, relativeFilePath);
+    destinationDirectoryPath = os.path.join(args.outputdirectory, relativeDirectoryPath);
     
+    #print("Resizing to Size: {1} Input: {0} Output: {2}".format(filePath, size, destinationFilePath));
+
+    if args.imagesize == "small":
+        outputWidth=640 #will be used if aspect ratio > 1.33
+        outputHeight=480 #will be used if aspect ratio <= 1.33
+    elif args.imagesize == "medium":
+        outputWidth=800 #will be used if aspect ratio > 1.33
+        outputHeight=600 #will be used if aspect ratio <= 1.33
+    else:
+        outputWidth=1000 #will be used if aspect ratio > 1.33
+        outputHeight=750 #will be used if aspect ratio <= 1.33
+
+    #using exact path because there is another program called "convert" in Windows
+    command = ["C:\Program Files\ImageMagick-7.0.6-Q16\convert.exe", filePath, "-define", "jpeg:extent=50kb", "-resize", str(outputWidth) + "x" + str(outputHeight), destinationFilePath];
+    print("Command: ", " ".join(command));
+
+    if not os.path.exists(destinationDirectoryPath):
+        print("Creating directory: " + destinationDirectoryPath);
+        os.makedirs(destinationDirectoryPath);
+        
+    run(command);
 
 def resizeVideoFile(path, subdirs, file, args):
     print("Dealing with the video file: ", path);
